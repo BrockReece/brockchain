61b36a5f15caff68a6f7af3884ab187d61c82d84
BrockReece <brockreece@gmail.com>
2019-07-07 16:09:32 +0100

    Key generation for authoring and signing changes

---
+++ src/keygen.js
@@ -0,0 +1,13 @@
+const es = require('./keys')
+
+// Generate a new key pair and convert them to hex-strings
+const key = ec.genKeyPair();
+const publicKey = key.getPublic('hex');
+const privateKey = key.getPrivate('hex');
+
+// Print the keys to the console
+console.log();
+console.log('Your public key (also your wallet address, freely shareable)\n', publicKey);
+
+console.log();
+console.log('Your private key (keep this secret! To sign transactions)\n', privateKey);
